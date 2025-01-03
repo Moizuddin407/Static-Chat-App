8ec8d869f1d41dd205e1016e089db020c0057848
Moiz Uddin M. Abdullah <moiz211j@gmail.com>
2025-01-03 12:14:39 +0500

    Adding a chat box front-end

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import '../App.css'
 export default function Chat() {
   return (
     <div>
-      <div className='Box'>
-        <h1>Hi</h1>
+      <div className='box'>
+        <h1>Hi there</h1>
+        <table>
+            <tr>
+                <td>
+                    Hey
+                </td>
+            </tr>
+        </table>
+        <button>
+            Submit
+        </button>
       </div>
     </div>
   )
